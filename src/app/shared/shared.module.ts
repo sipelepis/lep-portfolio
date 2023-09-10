7b1644fbeae11064d42f972b89eaf7132eb403ff
sipelepis <moran.phillipjan@ue.edu.ph>
2023-09-11 00:10:58 +0800

    Hiding Evidences

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from './components/carousel/carousel.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { ButtonComponent } from './components/button/button.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDividerModule } from '@angular/material/divider';
 import { CardComponent } from './components/card/card.component';
 import { CarouselModule } from 'primeng/carousel';
 import { MultiItemCarouselComponent } from './components/multi-item-carousel/multi-item-carousel.component';
